6a5b81f57c81e5ef04871719e1389eef98cf8ecd
0918nobita <nobita.0918@gmail.com>
2021-11-21 19:07:39 +0900

    :label: Transaction 型を定義した

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,7 @@
 import hashlib
 import json
 from time import time
-from typing import Optional, TypedDict
+from typing import TypedDict
 
 class Block(TypedDict):
     index: int
@@ -9,35 +9,51 @@ class Block(TypedDict):
     proof: int
     previous_hash: str
 
-class Blockchain:
-    def __init__(self):
-        self.chain: list[Block] = []
-        self.current_transactions = []
-        self.new_block(previous_hash=1, proof=100)
+def block_hash(block: Block) -> str:
+    """ブロックのハッシュを求める"""
+    encoded = json.dumps(block, sort_keys=True).encode()
+    return hashlib.sha256(encoded).hexdigest()
 
-    def new_block(self, proof: int, previous_hash: str | None = None) -> Block:
-        """新しいブロックを作り、チェーンに加える"""
-        block: Block = {
+class Transaction(TypedDict):
+    sender: str
+    recipient: str
+    amount: int
+
+class Blockchain:
+    def __init__(self) -> None:
+        self.chain: list[Block] = []
+        self.current_transactions: list[Transaction] = []
+        self.new_block(previous_hash="1", proof=100)
+
+    @property
+    def last_block(self) -> Block:
+        """チェーンの最後のブロックを返す"""
+        return self.chain[-1]
+
+    def new_block(self, proof: int, previous_hash: str | None = None) -> None:
+        """新しいブロックを作り、チェーンに加える
+
+        Args:
+            proof: PoW から得られるプルーフ
+            previous_hash: 前のブロックのハッシュ
+        """
+
+        del self.current_transactions[:] # 現在のトランザクションをリセット
+
+        self.chain.append({
             'index': len(self.chain) + 1,
             'timestamp': time(),
             'proof': proof,
-            'previous_hash': previous_hash or self.hash(self.chain[-1]),
-        }
-        del self.current_transactions[:]
-        self.chain.append(block)
-        return block
+            'previous_hash': previous_hash or block_hash(self.last_block),
+        })
 
-    def new_transaction(self):
-        """新しいトランザクションをリストに加える"""
-        pass
+    def new_transaction(self, transaction: Transaction) -> None:
+        """次に採掘されるブロックに加える新しいトランザクションを作る
+        
+        Args:
+            sender: 送信者のアドレス
+            recipient: 受信者のアドレス
+            amount: 量
+        """
 
-    @staticmethod
-    def hash(block: Block) -> str:
-        """ブロックをハッシュ化する"""
-        encoded = json.dumps(block, sort_keys=True).encode()
-        return hashlib.sha256(encoded).hexdigest()
-
-    @property
-    def last_block(self):
-        """チェーンの最後のブロックを返す"""
-        pass
+        self.current_transactions.append(transaction)
